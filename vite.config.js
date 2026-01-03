23197511d52a0dc9d8a7ee39cb303a979ed954ab
byte4byte <tiffintech365@gmail.com>
2026-01-04 00:20:40 +0530

    chore: add gh-pages and deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss() ],
+  base: '/Employee_Manegment_System/',
+  plugins: [react(), tailwindcss()],
 })
